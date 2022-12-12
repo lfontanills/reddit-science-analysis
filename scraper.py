60accd4667f43584317ee5ccf4cc410087549b16
Laura Fontanills <109815517+lfontanills@users.noreply.github.com>
2022-12-12 18:35:05 -0500

    updated file names, created file for drafting narrative

--- scraper.py
+++ scraper.py
@@ -6,9 +6,9 @@ import pandas
 
 # read-only instance
 reddit_read_only = praw.Reddit(
-    client_id=, #your info here
-    client_secret=, #your info here
-    user_agent=, #your info here
+    client_id="", #your info here
+    client_secret="", #your info here
+    user_agent="", #your info here
 )
 
 # extract subreddit information
@@ -26,30 +26,30 @@ print("Title:", subreddit.title)
 print("Description:", subreddit.description)
 
 # get top posts this from time period
-posts = subreddit.top("week")
+posts = subreddit.top("all")
 
 posts_dict = { 
-    "ID": [],
-    "Created UTC": [],
-    "Post URL": [],
-    "Title": [],
-    "Link flair text": [],
-    "Score": [],
-    "Num comments": [],
-    "Upvote ratio": []
+    "id": [],
+    "created_unix_utc": [],
+    "post_url": [],
+    "post_title": [],
+    "flair": [],
+    "score": [],
+    "num_comments": [],
+    "upvote_ratio": []
 }
 
 for post in posts:
-    posts_dict["ID"].append(post.id)
-    posts_dict["Created UTC"].append(post.created_utc)
-    posts_dict["Post URL"].append(post.url)
-    posts_dict["Title"].append(post.title)
-    posts_dict["Link flair text"].append(post.link_flair_text)
-    posts_dict["Score"].append(post.score)
-    posts_dict["Num comments"].append(post.num_comments)
-    posts_dict["Upvote ratio"].append(post.upvote_ratio)
+    posts_dict["id"].append(post.id)
+    posts_dict["created_unix_utc"].append(post.created_utc)
+    posts_dict["post_url"].append(post.url)
+    posts_dict["post_title"].append(post.title)
+    posts_dict["flair"].append(post.link_flair_text)
+    posts_dict["score"].append(post.score)
+    posts_dict["num_comments"].append(post.num_comments)
+    posts_dict["upvote_ratio"].append(post.upvote_ratio)
 
-top_posts_week = pandas.DataFrame(posts_dict)
-top_posts_week
+top_posts_all= pandas.DataFrame(posts_dict)
+top_posts_all
 
-top_posts_week.to_csv("Top-Posts-Week.csv")
+top_posts_all.to_csv("Top-Posts-All.csv")
