d4e4812ea2f4b0932c6879823e4f8facd706084c
lfontanills <109815517+lfontanills@users.noreply.github.com>
2022-12-12 15:54:51 -0500

    commit before major revisions

--- cleaning.R
+++ cleaning.R
@@ -5,7 +5,6 @@ library(ggplot2)
 library(urltools) #package wrangles urls
 library(tidytext) #NLP 
 
-
 # import top posts - last month
 library(readr)
 Top_Posts_Month <- read_csv("Top Posts Month.csv")
@@ -27,10 +26,15 @@ Top_Posts_Month$`Created utc` <- as_datetime(Top_Posts_Month$`Created unix`)
 # create new column with domain names
 Top_Posts_Month$`Domain` <- domain(Top_Posts_Month$`Post URL`)
 
-# create new column with titles
-Top_Posts_Month$`Title Clean` <- Top_Posts_Month$`Title`
 
-# make titles lowercase
-Top_Posts_Month$`Title Clean` <- tolower(Top_Posts_Month$`Title Clean`)
+# clean titles: remove case, punctuation, numerics, whitespace
+Top_Posts_Month$`Clean Title` <- tolower(Top_Posts_Month$Title)
+Top_Posts_Month$`Clean Title` <- gsub("[[:punct:]]", "", Top_Posts_Month$`Clean Title`)
+Top_Posts_Month$`Clean Title` <- gsub("[[:digit:]]", "", Top_Posts_Month$`Clean Title`)
+Top_Posts_Month$`Clean Title` <- gsub("\\s+", " ", str_trim(Top_Posts_Month$`Clean Title`))
+
+  
+  
+
 
 
