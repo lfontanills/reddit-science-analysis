fcc610a92d1fb0d7a8989d78b25720995a1eaafb
Laura Fontanills <109815517+lfontanills@users.noreply.github.com>
2022-12-12 20:14:21 -0500

    cleaned datetime

--- cleaning.R
+++ cleaning.R
@@ -7,13 +7,24 @@ library(tidytext) #NLP
 
 # change column 1 name from ...1 to all_time_rank, year_rank, or month_rank
 colnames(top_all)[1] <- "all_time_rank"
+
 colnames(top_year)[1] <- "year_rank"
+
 colnames(top_month)[1] <- "month_rank"
 
 # add 1 to all the rankings for clarity
 top_all$all_time_rank = top_all$all_time_rank + 1
+
 top_year$year_rank = top_year$year_rank + 1
+
 top_month$month_rank = top_month$month_rank + 1
+
+# change created_unix_utc to a datetime
+top_all$created_utc <- as_datetime(top_all$created_unix_utc)
+
+top_year$created_utc <- as_datetime(top_year$created_unix_utc)
+
+top_month$created_utc <- as_datetime(top_month$created_unix_utc)
 
 # combine data frames
 top_post_temp <- full_join(top_all, top_year)
