e0e32a6ac3c39718f6c5ce450d8426b4b2340cd8
Laura Fontanills <109815517+lfontanills@users.noreply.github.com>
2022-12-13 11:38:31 -0500

    consolidated scripts

--- wrangling_cleaning.R
+++ wrangling_cleaning.R
@@ -5,6 +5,22 @@ library(ggplot2)
 library(urltools) #package wrangles urls
 library(tidytext) #NLP 
 
+#import packages
+library(readr)
+
+# create and view data frame with top 100 posts from the last month
+top_month <- read_csv("Top-Posts-Month.csv")
+View(top_month)
+
+# create and view data frame with top 100 posts from the last year
+top_year <- read_csv("Top-Posts-Year.csv")
+View(top_year)
+
+# create view data frame with top 100 posts from all time
+top_all <- read_csv("Top-Posts-All.csv")
+View(top_all)
+
+
 # change column 1 name from ...1 to all_time_rank, year_rank, or month_rank
 colnames(top_all)[1] <- "all_time_rank"
 
