361bafea32c490feaa4f6606dd33313b24c6d778
lfontanills <109815517+lfontanills@users.noreply.github.com>
2022-12-14 14:25:27 -0500

    add word frequencies calculation

--- text_wrangling.R
+++ text_wrangling.R
@@ -3,11 +3,12 @@
 library(tidyverse) # data cleaning
 library(tidytext) #NLP toolkit
 
+
 # subset dataframes with title text, flair, domain
 
-text_month <- top_month[,c("month_rank","id", "flair", "domain", "post_title")]
-text_year <- top_year[,c("year_rank","id", "flair", "domain", "post_title")]
-text_all <- top_all[,c("all_time_rank","id", "flair", "domain", "post_title")]
+text_month <- top_month[,c("post_title")]
+text_year <- top_year[,c("post_title")]
+text_all <- top_all[,c("post_title")]
 
 # restructure one token per row: unnest tokens
 tidy_month <-text_month %>% 
@@ -45,3 +46,21 @@ tidy_year %>%
 
 tidy_all %>% 
   count(word, sort = TRUE)
+
+# calculate word frequencies
+
+frequency <- bind_rows(mutate(tidy_month, timeframe = "month"),
+                       mutate(tidy_year, timeframe = "year"),
+                       mutate(tidy_all, timeframe = "all time")) %>% 
+  mutate(word = str_extract(word, "[a-z']+")) %>% 
+  count(timeframe, word) %>% 
+  group_by(timeframe) %>% 
+  mutate(proportion = n / sum(n)) %>% 
+  select(-n) %>% 
+  pivot_wider(names_from = timeframe, values_from = proportion) %>% 
+  pivot_longer(`month`:`year`,
+               names_to = "timeframe", values_to = "proportion")
+  
+frequency
+  
+ 
