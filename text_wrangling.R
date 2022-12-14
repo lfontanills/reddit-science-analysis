8f458f16308441ca0f800df72f8a6fa22a39e939
lfontanills <109815517+lfontanills@users.noreply.github.com>
2022-12-14 13:52:00 -0500

    unnest tokens

--- text_wrangling.R
+++ text_wrangling.R
@@ -5,4 +5,19 @@ library(tidytext) #NLP toolkit
 
 # subset dataframes with title text, flair, domain
 
-text_month <- top_month %>% 
+text_month <- top_month[,c("month_rank","id", "flair", "domain", "post_title")]
+text_year <- top_year[,c("year_rank","id", "flair", "domain", "post_title")]
+text_all <- top_all[,c("all_time_rank","id", "flair", "domain", "post_title")]
+
+# restructure one token per row: unnest tokens
+tidy_month <-text_month %>% 
+  unnest_tokens(word, post_title)
+tidy_month
+
+tidy_year <- text_year %>% 
+  unnest_tokens(word, post_title)
+tidy_year
+
+tidy_all <- text_all %>% 
+  unnest_tokens(word, post_title)
+tidy_all
