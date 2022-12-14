d249a623d147c3338a6c1a808879534b4f0b9132
lfontanills <109815517+lfontanills@users.noreply.github.com>
2022-12-14 15:17:37 -0500

    lots of wordclouds

--- text_wrangling.R
+++ text_wrangling.R
@@ -1,14 +1,16 @@
 #import packages for project
 
 library(tidyverse) # data cleaning
-library(tidytext) #NLP toolkit
+library(tidytext) # NLP toolkit
+library(textdata) # Sentiment analysis
+library(wordcloud) # Wordclouds
 
 
 # subset dataframes with title text, flair, domain
 
 text_month <- top_month[,c("post_title")]
 text_year <- top_year[,c("post_title")]
-text_all <- top_all[,c("post_title")]
+text_all <- top_all[,c("all_time_rank", "flair","post_title")]
 
 # restructure one token per row: unnest tokens
 tidy_month <-text_month %>% 
@@ -85,3 +87,96 @@ cor.test(data = frequency[frequency$timeframe == "month",],
 
 cor.test(data = frequency[frequency$timeframe == "year",],
          ~ proportion + `all time`)
+
+# sentiment analysis
+
+get_sentiments("afinn")
+get_sentiments("bing")
+get_sentiments("nrc")
+
+all_time_sentiment <- tidy_all %>% 
+  inner_join(get_sentiments("nrc")) %>% 
+  count(flair, index = all_time_rank %/% 80, sentiment) %>% 
+  pivot_wider(names_from = sentiment, values_from = n, values_fill = 0) %>% 
+  mutate(sentiment = positive - negative)
+all_time_sentiment
+
+afinn <- tidy_all %>% 
+  inner_join(get_sentiments("afinn")) %>% 
+  group_by(index = all_time_rank %/% 80) %>% 
+  mutate(method = "AFINN")
+  
+bing_and_nrc <- bind_rows(
+  tidy_all %>% 
+    inner_join(get_sentiments("bing")) %>% 
+   mutate(method = "Bing et al."),
+  tidy_all %>% 
+    inner_join(get_sentiments("nrc") %>% 
+                 filter(sentiment %in% c("positive", "negative"))
+               ) %>% 
+    mutate(method = "NRC")) %>% 
+  count(method, index = all_time_rank %/% 80, sentiment) %>% 
+  pivot_wider(names_from = sentiment, values_from = n, values_fill = 0) %>% 
+  mutate(sentiment = positive - negative)
+  
+
+  # plot sentiment analysis
+
+bind_rows(afinn,
+          bing_and_nrc) %>% 
+  ggplot(aes(index, sentiment, fill = method)) + 
+  geom_col(show.legend = FALSE) + 
+  facet_wrap(~method)
+
+
+get_sentiments("nrc") %>% 
+  filter(sentiment %in% c("positive", "negaative")) %>% 
+  count(sentiment)
+
+get_sentiments("bing") %>% 
+  count(sentiment)
+
+get_sentiments("afinn") %>% 
+  count(value)
+
+# most common +, - words
+
+bing_word_counts <- tidy_all %>% 
+  inner_join(get_sentiments("bing")) %>% 
+  count(word, sentiment, sort = TRUE) %>% 
+  ungroup()
+
+bing_word_counts %>% 
+  group_by(sentiment) %>% 
+  slice_max(n, n=10) %>% 
+  ungroup() %>% 
+  mutate(word = reorder(word,n)) %>% 
+  ggplot(aes(n, word, fill = sentiment)) +
+  geom_col(show.legend = FALSE) + 
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(x = "Contribution to sentiment",
+       y = NULL)
+
+custom_stop_words <- bind_rows(tibble(word = c(1:2022, "study", "found", "percent", "researchers", "research", "suggests", "suggest", "scientist", "scientists"),
+                                      lexicon = c("custom")),
+                               stop_words)
+
+
+# wordclouds - fix scale later
+
+tidy_all %>% 
+  anti_join(custom_stop_words) %>% 
+  count(word) %>% 
+  with(wordcloud(word, n, max.words = 50))
+
+tidy_year %>% 
+  anti_join(custom_stop_words) %>% 
+  count(word) %>% 
+  with(wordcloud(word, n, max.words = 50))
+
+tidy_month %>% 
+  anti_join(custom_stop_words) %>% 
+  count(word) %>% 
+  with(wordcloud(word, n, max.words = 50))
+
+
