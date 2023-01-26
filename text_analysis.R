1d8f4ca41930a13d1beeb9acf4ac8670685d4ed5
lfontanills <109815517+lfontanills@users.noreply.github.com>
2023-01-26 15:50:27 -0500

    Consolidated text analysis into one file

--- text_analysis.R
+++ text_analysis.R
@@ -16,15 +16,12 @@ text_all <- top_all[c("post_title")]
 # restructure one token per row: unnest tokens
 tidy_month <-text_month %>% 
   unnest_tokens(word, post_title)
-tidy_month
 
 tidy_year <- text_year %>% 
   unnest_tokens(word, post_title)
-tidy_year
 
 tidy_all <- text_all %>% 
   unnest_tokens(word, post_title)
-tidy_all
 
 # remove stop words
 
@@ -210,17 +207,116 @@ bing_word_counts %>%
 
 
 
-# wordclouds - fix scale later
+# wordclouds
 
-tidy_all %>% 
-  count(word) %>% 
-  with(wordcloud(word, n, colors=colorRampPalette(brewer.pal(9,"BuPu"))(50), max.words = 30))
+all_words <- tidy_all %>% count(word, sort=TRUE)
+wordcloud2(all_words, size = 1.6, )
 
-tidy_year %>% 
-  count(word) %>% 
-  with(wordcloud(word, n, colors=colorRampPalette(brewer.pal(9,"BuPu"))(50), max.words = 30))
+year_words <- tidy_year %>% count(word, sort=TRUE)
+wordcloud2(year_words, size = 1.6, color = (c("green","blue")))
 
-tidy_month %>% 
-  count(word) %>% 
-  with(wordcloud(word, n, colors=colorRampPalette(brewer.pal(9,"BuPu"))(50), max.words = 30))
+month_words <- tidy_month %>% count(word, sort=TRUE)
+wordcloud2(month_words, size = 1.6, color=(c("purple","blue")))
+
+
+### Compare to journal news sites - homepages
+
+nature_titles <- read_csv("nature-titles.csv")
+
+# restructure one token per row: unnest tokens
+tidy_nature <-nature_texts %>% 
+  unnest_tokens(word, titles)
+
+# remove stop words
+data("stop_words")
+
+# customize stop words
+
+tidy_nature <- tidy_nature %>% 
+  anti_join(custom_stop_words)
+
+
+nature_words <- tidy_nature %>% 
+  count(word, sort = TRUE) 
+
+## add more data points from popular science psych/social science source
+library(readr)
+frontiers_titles <- read_csv("frontiers-titles.csv")
+
+# restructure one token per row: unnest tokens
+tidy_frontiers <-frontiers_titles %>% 
+  unnest_tokens(word, titles)
+
+# remove stop words
+tidy_frontiers <- tidy_frontiers %>% 
+  anti_join(custom_stop_words)
+
+
+frontiers_words <- tidy_frontiers %>% 
+  count(word, sort = TRUE)
+
+# word clouds
+
+nature_words <- tidy_nature %>% count(word, sort=TRUE)
+wordcloud2(nature_words, size = 1.6, color=(c("red","pink")))
+
+frontiers_words <- tidy_frontiers %>% count(word, sort=TRUE)
+wordcloud2(frontiers_words, size = 1.6, color=(c("red","purple")))
+
+
+# calculate frequency
+
+frequency_compare <- bind_rows(mutate(tidy_all, category = "reddit"),
+                               mutate(tidy_nature, category = "nature"),
+                               mutate(tidy_frontiers, category = "frontiers")) %>% 
+  mutate(word = str_extract(word, "[a-z']+")) %>% 
+  count(category, word) %>% 
+  group_by(category) %>% 
+  mutate(proportion = n / sum(n)) %>% 
+  select(-n) %>% 
+  pivot_wider(names_from = category, values_from = proportion) %>% 
+  pivot_longer(`nature`:`frontiers`,
+               names_to = "category", values_to = "proportion")
+
+# see frequency of top reddit words in journal blogs
+
+frequency_compare %>% 
+  filter (word != "NA") %>% 
+  filter(category == "nature") %>% 
+  arrange(desc(reddit)) %>% 
+  print(n=20)
+
+frequency_compare %>% 
+  filter (word != "NA") %>% 
+  filter(category == "frontiers") %>% 
+  arrange(desc(reddit)) %>% 
+  print(n=20)
+
+# correlation tests
+cor.test(data = frequency_compare[frequency_compare$category == "nature",],
+         ~ proportion + `reddit`)
+
+cor.test(data = frequency_compare[frequency_compare$category == "frontiers",],
+         ~ proportion + `reddit`)
+
+# plot frequencies
+
+ggplot(frequency_compare, aes(x = proportion, y = `reddit`, 
+                              color = abs(`reddit` - proportion))) +
+  geom_abline(color = "gray40", lty = 2) +
+  geom_jitter(alpha = 0.1, size = 2.5, width = 0.3, height = 0.3) +
+  geom_text(aes(label = word), check_overlap = TRUE, vjust = 1.5) +
+  scale_x_log10() +
+  scale_y_log10() +
+  scale_color_gradient(limits = c(0, 0.001), 
+                       low = "darkslategray4", high = "gray75") +
+  facet_wrap(~category, ncol = 2) +
+  theme(legend.position="none") +
+  labs(y = "Reddit", x = NULL)
+
+# export to csv
+
+write.csv(tidy_nature, file = "~/Documents/Projects/reddit-science-analysis-2/tidy_nature.csv")
+write.csv(tidy_frontiers, file = "~/Documents/Projects/reddit-science-analysis-2/tidy_frontiers.csv")
+
 
