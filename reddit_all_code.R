efccc5c3edc1d94236c47cb98f5358c328c93396
lfontanills <109815517+lfontanills@users.noreply.github.com>
2023-01-27 21:42:19 -0500

    Initial commit

---
+++ reddit_all_code.R
@@ -0,0 +1,71 @@
+## Data cleaning
+
+#import packages for project
+library(tidyverse)
+library(lubridate)
+library(skimr)
+library(urltools) #package wrangles urls
+
+# create view data frame with top 100 posts from all time
+top_all <- read_csv("Top-Posts-All.csv")
+
+# create and view data frame with top 100 posts from the last year
+top_year <- read_csv("Top-Posts-Year.csv")
+
+# create and view data frame with top 100 posts from the last month
+top_month <- read_csv("Top-Posts-Month.csv")
+
+
+# check that all rows are unique
+
+n_distinct(top_all$id) == nrow(top_all)
+n_distinct(top_year$id) == nrow(top_year)
+n_distinct(top_month$id) == nrow(top_month)
+
+# change column 1 name from ...1 to rank
+colnames(top_all)[1] <- "all_rank"
+colnames(top_year)[1] <- "year_rank"
+colnames(top_month)[1] <- "month_rank"
+
+glimpse(top_all)
+
+# add 1 to all the rankings for clarity
+top_all$all_rank = top_all$all_rank + 1
+
+top_year$year_rank = top_year$year_rank + 1
+
+top_month$month_rank = top_month$month_rank + 1
+
+# check post rankings are between 1 and 100
+
+summary(top_all)
+summary(top_year)
+summary(top_month)
+
+# change created_unix_utc to a datetime
+top_all$created_utc <- as_datetime(top_all$created_unix_utc)
+
+top_year$created_utc <- as_datetime(top_year$created_unix_utc)
+
+top_month$created_utc <- as_datetime(top_month$created_unix_utc)
+
+# make column with domain name only
+top_all$domain <- domain(top_all$post_url)
+
+top_year$domain <- domain(top_year$post_url)
+
+top_month$domain <- domain(top_month$post_url)
+
+head(top_all)
+
+# make cleaned dfs
+
+all_clean <- top_all
+year_clean <- top_year
+month_clean <- top_month
+
+# export cleaned files as csvs
+write.csv(all_clean, file = "~/Documents/Projects/reddit-science/all_clean.csv")
+write.csv(year_clean, file = "~/Documents/Projects/reddit-science/year_clean.csv")
+write.csv(month_clean, file = "~/Documents/Projects/reddit-science/month_clean.csv")
+
